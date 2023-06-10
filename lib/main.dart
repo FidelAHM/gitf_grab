55dbc4a914a569166656dbb817d024d0dd545301
Fidel AHM <fidelahm10@gmail.com>
2023-06-09 21:31:34 -0700

    gift grab

---
+++ lib/main.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'package:gift_grab/screens/game_play.dart';
+import 'package:gift_grab/services/hive_service.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await HiveService.openHiveBox(boxName: 'settings');
+
+  runApp(
+    const MaterialApp(
+      home: GamePlay(),
+    ),
+  );
+}
